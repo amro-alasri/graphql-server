620ee1cdfdc75a34d1333907cb0fc7c582f1cf0e
Amro Alasri <98550418+amro-alasri@users.noreply.github.com>
2024-01-27 22:50:53 +0300

    init commit

---
+++ graph/resolver.go
@@ -0,0 +1,11 @@
+package graph
+
+import "github.com/amro-alasri/graphQL-server/graph/model"
+
+// This file will not be regenerated automatically.
+//
+// It serves as dependency injection for your app, add any dependencies you require here.
+
+type Resolver struct {
+	videos []*model.Video
+}
